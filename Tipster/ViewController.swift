eb8f75c8d880a3b016e7223f2a154bb89fa1c7ef
Russell <russelllauletta@pivotal-guest-94.sf.pivotallabs.com>
2015-09-26 10:26:29 -0700

    Initial Commit

---
+++ Tipster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tipster
+//
+//  Created by Russell Lauletta on 9/26/15.
+//  Copyright © 2015 Russell Lauletta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
